1a4fc8563a8c9244f00c658c8ab4ea10f007ddd4
alimmondal <italimbd@gmail.com>
2023-08-15 14:53:30 +0600

    upd

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -14,27 +14,44 @@ const createPost = async (req: Request, res: Response) => {
   }
 };
 
-// const createOrUpdateProfile = async (req: Request, res: Response) => {
-//   try {
-//     const result = await UserService.insertOrUpdateProfile(req.body);
-//     res.send({
-//       success: true,
-//       message: "Profile inserted/ updated successfully",
-//       data: result,
-//     });
-//   } catch (error) {
-//     res.send(error);
-//   }
-// };
-
 const getAllPosts = async (req: Request, res: Response) => {
-  console.log(req.query);
+  // console.log(req.query);
   const options = req.query;
   try {
     const result = await PostService.getPostFromDb(options);
     res.send({
       success: true,
       message: "Posts retrieved successfully",
+      total: result.total,
+      data: result.data,
+    });
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+const updatePost = async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  const data = req.body;
+  try {
+    const result = await PostService.updatePost(id, data);
+    res.send({
+      success: true,
+      message: "Posts updated successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+const deletePost = async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  try {
+    const result = await PostService.deletePost(id);
+    res.send({
+      success: true,
+      message: "Posts deleted successfully",
       data: result,
     });
   } catch (error) {
@@ -58,7 +75,9 @@ const getSinglePost = async (req: Request, res: Response) => {
 };
 
 export const PostController = {
+  updatePost,
   createPost,
   getAllPosts,
   getSinglePost,
+  deletePost,
 };
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -13,43 +13,52 @@ const insertPostIntoDb = async (data: Post): Promise<Post> => {
   return result;
 };
 
-// const insertOrUpdateProfile = async (data: Post): Promise<Post> => {
-//   const isExist = await prisma.profile.findUnique({
-//     where: {
-//       userId: data.userId,
-//     },
-//   });
-
-//   if (isExist) {
-//     const result = await prisma.post.update({
-//       where: { userId: data.userId },
-//       data: {
-//         bio: data.bio,
-//       },
-//     });
-//     return result;
-//   }
-
-//   const result = await prisma.post.create({ data });
-//   return result;
-// };
-
 const getPostFromDb = async (options: any) => {
-  const { sortBy, sortOrder } = options;
-  const result = await prisma.post.findMany({
-    // select: {
-    //   email: true,
-    //   name: true,
-    // },
-    include: {
-      category: true,
-      author: true,
-    },
-    orderBy: {
-      [sortBy]: sortOrder,
-    },
+  const { sortBy, sortOrder, searchTerm, page, limit } = options;
+  const skip = parseInt(limit) * parseInt(page) - parseInt(limit);
+
+  const take = parseInt(limit);
+
+  return await prisma.$transaction(async (tx) => {
+    const result = await tx.post.findMany({
+      skip,
+      take,
+      // select: {
+      //   email: true,
+      //   name: true,
+      // },
+      include: {
+        category: true,
+        author: true,
+      },
+      orderBy:
+        sortBy && sortOrder
+          ? {
+              [sortBy]: sortOrder,
+            }
+          : { createdAt: "asc" },
+      where: {
+        OR: [
+          {
+            title: {
+              contains: searchTerm,
+              mode: "insensitive",
+            },
+          },
+          {
+            author: {
+              name: {
+                contains: searchTerm,
+                mode: "insensitive",
+              },
+            },
+          },
+        ],
+      },
+    });
+    const total = await tx.post.count();
+    return { total: total, data: result };
   });
-  return result;
 };
 
 const getSinglePostFromDb = async (id: number) => {
@@ -65,8 +74,38 @@ const getSinglePostFromDb = async (id: number) => {
   return result;
 };
 
+const updatePost = async (
+  id: number,
+  payload: Partial<Post>
+): Promise<Post> => {
+  const result = await prisma.post.update({
+    where: { id },
+    data: payload,
+  });
+  return result;
+};
+
+const deletePost = async (id: number): Promise<Post> => {
+  const result = await prisma.post.delete({
+    where: { id },
+  });
+  return result;
+};
+
 export const PostService = {
   insertPostIntoDb,
   getPostFromDb,
   getSinglePostFromDb,
+  updatePost,
+  deletePost,
 };
+
+/*
+ *limit = 5
+ *page = 1
+ *total = 10
+ *take = limit
+ *skip = limit * page - limit
+ *      =5*1-5=0
+ * 1 2 3 4 5 6 7 8 9 10 11
+ */
